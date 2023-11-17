04ffd8ad83ddda624a4484abb9608157a6bfe387
tsiupaknazar <sjprime280@gmail.com>
2023-11-17 22:00:53 +0000

    Implemented Quill instead of TinyMCE

--- src/components/text-editor/quilleditor.jsx
+++ src/components/text-editor/quilleditor.jsx
@@ -1,37 +1,41 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import ReactQuill from "react-quill";
-
 import "react-quill/dist/quill.snow.css";
-import "react-quill/dist/quill.bubble.css";
 
-const QuillEditor = () => {
-  const [editorValue, setEditorValue] = useState("");
+const QuillEditor = ({ editorRef, initialValue }) => {
+  const [editorValue, setEditorValue] = useState(initialValue || "");
+  const quillRef = useRef(null);
+
+  useEffect(() => {
+    if (editorRef) {
+      editorRef(quillRef.current);
+    }
+  }, [editorRef]);
+
+  const modules = {
+    toolbar: [
+      [{ header: 1 }, { header: 2 }],
+      [{ list: "ordered" }, { list: "bullet" }],
+      [{ script: "sub" }, { script: "super" }],
+      [{ direction: "rtl" }],
+      ["bold", "italic", "underline", "strike"],
+      ["blockquote", "code-block"],
+      [{ size: ["small", false, "large", "huge"] }],
+      [{ header: [1, 2, 3, 4, 5, 6, false] }],
+      [{ color: [] }, { background: [] }],
+      [{ font: [] }],
+      [{ align: [] }],
+      ["clean"],
+    ],
+  };
+
   return (
     <ReactQuill
+      ref={quillRef}
       value={editorValue}
       onChange={(value) => setEditorValue(value)}
-      modules={{
-        toolbar: [
-          ["bold", "italic", "underline", "strike"], // toggled buttons
-          ["blockquote", "code-block"],
-
-          [{ header: 1 }, { header: 2 }, { header: 3 }], // custom button values
-          [{ list: "ordered" }, { list: "bullet" }],
-          [{ script: "sub" }, { script: "super" }], // superscript/subscript
-          [{ indent: "-1" }, { indent: "+1" }], // outdent/indent
-          [{ direction: "rtl" }], // text direction
-
-          [{ size: ["small", false, "large", "huge"] }], // custom dropdown
-          [{ header: [1, 2, 3, 4, 5, 6, false] }],
-
-          [{ color: [] }, { background: [] }], // dropdown with defaults from theme
-          [{ font: [] }],
-          [{ align: [] }],
-
-          ["clean"],
-        ],
-      }}
-      className="w-full border-2 border-red-700 ql-toolbar ql-icon"
+      modules={modules}
+      className="w-full md:h-[76vh] h-[73vh]"
     />
   );
 };
--- src/pages/Editor.jsx
+++ src/pages/Editor.jsx
@@ -8,14 +8,14 @@ import { doc, getDoc, updateDoc } from "@firebase/firestore";
 import Header from "../components/header/Header";
 import { AuthContext } from "../context/AuthContext";
 
-import { Editor } from "@tinymce/tinymce-react";
 import { ToastContainer, toast } from "react-toastify";
 import Loader from "../components/loader/Loader";
 import QuillEditor from "../components/text-editor/quilleditor";
+import { convertToHTML } from "draft-convert";
 
 const EditorPage = () => {
   const { user } = useContext(AuthContext);
-  const { theme } = useContext(ThemeContext);
+  // const { theme } = useContext(ThemeContext);
   const [userDoc, setUserDoc] = useState(null);
   const [loading, setLoading] = useState(true);
 
@@ -32,7 +32,19 @@ const EditorPage = () => {
         "docs",
         `${id}`
       );
-      await updateDoc(docRef, { content: editorRef.current.getContent() });
+
+      // Get the plain content from the Delta object
+      const content = editorRef.current?.getEditor().getContents();
+
+      // Convert the Delta object to a plain JavaScript object
+      const contentObject = content ? content.ops : [];
+
+      // Convert the plain object to HTML
+      const htmlContent = convertToHTML({ content: contentObject });
+
+      // Update the document with the HTML content
+      await updateDoc(docRef, { content: htmlContent });
+
       toast.success("Document successfully saved!");
     } catch (error) {
       toast.error("Error saving document");
@@ -54,13 +66,13 @@ const EditorPage = () => {
         const data = docSnap.data();
         setUserDoc(data);
         if (data.content) {
-          editorRef.current.setContent(data?.content);
+          editorRef.current?.getEditor().setContents(data.content);
         }
       }
       setLoading(false);
     };
     getUserDoc();
-  }, [id, user?.uid]);
+  }, [id, user?.uid, editorRef]);
 
   return (
     <>
@@ -115,7 +127,10 @@ const EditorPage = () => {
               `,
             }}
           /> */}
-          <QuillEditor />
+          <QuillEditor
+            editorRef={(ref) => (editorRef.current = ref)}
+            initialValue={userDoc?.content || ""}
+          />
         </div>
         <ToastContainer hideProgressBar={true} autoClose={3000} />
       </div>
